b32e69aab75d1f95b3dcf2d38f7a09dcb70ee38c
dreipiano <150403877+dreipiano@users.noreply.github.com>
2024-09-27 08:14:50 +0800

    Update dissolve.py
    Added some commented lines which were used for directly testing out the script.

--- dissolve.py
+++ dissolve.py
@@ -1,4 +1,4 @@
-import geopandas as gpd
+import geopandas as gpd    # Used only for debugging
 
 # Loop through all files in the input folder
 def dissolve(input_file):
@@ -6,3 +6,7 @@ def dissolve(input_file):
     # Dissolve all features into one
     gdf = input_file.dissolve()
     return gdf
+
+# gdf = gpd.read_file("")
+# gdf = dissolve_poly(gdf)
+# gdf.to_file("")
